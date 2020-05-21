b6346b66e1733264082ad207bd56cdd584172563
m9dnts8608 <leah8608@gmail.com>
2020-05-21 21:05:41 +0900

    ~ install pug

---
+++ routers/globalRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import routes from "../routes";
+import { home, search, videos } from "../controllers/videoController";
+import { join, login, logout, users } from "../controllers/userController";
+import userRouter from "./userRouter";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+
+globalRouter.get(routes.users, users);
+globalRouter.get(routes.videos, videos);
+
+export default globalRouter;
